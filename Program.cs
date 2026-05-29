f8e93714c9b057d487eff6fd706beb338c714b71
yasit <yasit@DESKTOP-70P6T6I>
2026-05-29 20:27:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        int[] nums = new int[] { 1, -2, 3, 4, -5, 60, -700 };
+        Console.WriteLine("Исходный массив");
+        foreach (int i in nums)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Отсортированный массив в порядке возрастания");
+        Array.Sort(nums);
+        foreach (int i in nums)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Сортировка массива в порядке убывания");
+        Array.Reverse(nums);
+        foreach (int i in nums)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
